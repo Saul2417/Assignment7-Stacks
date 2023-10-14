59e694170196887f225fa0cd785edf725b6eee29
Saul2417 <compadre2001@gmail.com>
2023-10-14 14:49:08 -0700

    An initial implementation: The game seems to function as intended, need help in figuring out how to display anything

---
+++ Assignment7-Stacks/n_Queens.cpp
@@ -0,0 +1,306 @@
+#include "n_Queens.h"
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "input.h"
+
+bool checkIfConflict(int boardSize, stack<BoardSpaces> filledSpaces, BoardSpaces currentSpace);
+
+n_Queens::n_Queens()
+{
+	shiftDirection = 'R';
+	firstColumnPermutaions = 0;
+	boardSize = 0;
+	filled = 0;
+	gameWon = false;
+}
+
+void n_Queens::setBoardSize(int newBoardSize)
+{
+	boardSize = newBoardSize;
+}
+
+void n_Queens::setFilled(int newFilledAmount)
+{
+	filled = newFilledAmount;
+}
+
+int n_Queens::getBoardSize() const
+{
+	return boardSize;
+}
+
+int n_Queens::getFilled() const
+{
+	return filled;
+}
+
+void n_Queens::fillBoard()
+{
+	system("cls");
+	BoardSpaces currentSpace;
+	bool queenConflict = false;
+	boardSize = inputInteger("Enter a number(1..100) of queens: ", 1, 100);
+	currentSpace.setColumn(inputInteger("Enter the column to place the first queen : ", 1, boardSize));
+	filledSpaces.push(currentSpace);
+	firstColumnPermutaions++;
+	do
+	{
+		queenConflict = checkIfConflict(boardSize, filledSpaces, currentSpace);
+		if (queenConflict == false)
+		{
+			filled++;
+			if (filled == boardSize)
+			{
+				gameWon = true;
+				displayBoard();
+				break;
+			}
+			else
+			{
+				currentSpace.setRow(currentSpace.getRow() + 1);
+				currentSpace.setColumn(1);
+				filledSpaces.push(currentSpace);
+			}
+		}
+		else if (currentSpace.getColumn() < boardSize)
+		{
+			currentSpace.setColumn(filledSpaces.top().getColumn() + 1);
+			filledSpaces.top() = currentSpace;
+			
+		}
+		else if (currentSpace.getColumn() >= boardSize)
+		{
+			while (filledSpaces.size() > 1)
+			{
+				if (filledSpaces.top().getColumn() < boardSize)
+				{
+					currentSpace.setRow(filledSpaces.top().getRow());
+					currentSpace.setColumn(filledSpaces.top().getColumn() + 1);
+					filledSpaces.top() = currentSpace;
+					break;
+				}
+				filledSpaces.pop();
+				filled--;				
+			}
+			if (filledSpaces.top().getRow() == 1)
+			{
+				if (firstColumnPermutaions == boardSize)
+				{
+					cout << "No Solution Found. " << endl;
+					return;
+				}
+				else if (filledSpaces.top().getColumn() == boardSize)
+				{
+					filledSpaces.top().setColumn(boardSize - firstColumnPermutaions);
+					currentSpace.setRow(1);
+					currentSpace.setColumn(filledSpaces.top().getColumn());
+					shiftDirection = 'L';
+				}
+				else
+				{
+					currentSpace.setRow(1);
+					if (shiftDirection == 'L')
+					{
+						currentSpace.setColumn(filledSpaces.top().getColumn() - 1);
+					}
+					currentSpace.setColumn(filledSpaces.top().getColumn() + 1);
+					filledSpaces.top() = currentSpace;
+					firstColumnPermutaions++;
+				}
+				
+			}		
+		}
+
+		//if (filledSpaces.empty())
+		//{
+		//	filledSpaces.push(currentSpace);
+		//	filled++;
+		//	if (filled == boardSize)
+		//	{
+		//		gameWon = true;
+		//		displayBoard();
+		//		break;
+		//	}
+		//	else
+		//	{
+		//		currentSpace.setRow(currentSpace.getRow() + 1);
+		//		currentSpace.setColumn(1);
+		//	}
+		//}
+		//else
+		//{
+		//	if (checkIfConflict(boardSize, filledSpaces, currentSpace) == false)
+		//	{
+		//		filledSpaces.push(currentSpace);
+		//		filled++;
+		//		if (filled == boardSize)
+		//		{
+		//			gameWon = true;
+		//			displayBoard();
+		//			break;
+		//		}
+		//		else
+		//		{
+		//			currentSpace.setRow(currentSpace.getRow() + 1);
+		//			currentSpace.setColumn(1);
+		//		}
+		//	}
+		//	/*else if ((currentSpace.getRow() == boardSize) && (currentSpace.getColumn() == boardSize) && (filled == boardSize - 1))
+		//	{
+		//		std::cout << "No Solution Found. " << endl;
+		//		return;
+		//	}*/
+		//	else if (currentSpace.getColumn() >= boardSize)
+		//	{
+		//		currentSpace.setRow(filledSpaces.top().getRow());
+		//		currentSpace.setColumn(filledSpaces.top().getColumn() + 1);
+		//		filledSpaces.pop();
+		//		filled--;				
+		//	}
+		//	else if (currentSpace.getRow() <= boardSize)
+		//	{
+		//		currentSpace.setColumn(currentSpace.getColumn() + 1);
+		//	}		
+		//}
+		// 
+		// 
+						
+	} while (gameWon == false);
+}
+
+//if (filledSpaces.empty())
+//{
+//	filledSpaces.push(currentSpace);
+//	filled++;
+//	if (filled == boardSize)
+//	{
+//		gameWon = true;
+//		displayBoard();
+//		break;
+//	}
+//	else
+//	{
+//		currentSpace.setRow(currentSpace.getRow() + 1);
+//		currentSpace.setColumn(1);
+//	}
+//}
+//else
+//{
+//	if (checkIfConflict(boardSize, filledSpaces, currentSpace) == false)
+//	{
+//		filledSpaces.push(currentSpace);
+//		filled++;
+//		if (filled == boardSize)
+//		{
+//			gameWon = true;
+//			displayBoard();
+//			break;
+//		}
+//		else
+//		{
+//			currentSpace.setRow(currentSpace.getRow() + 1);
+//			currentSpace.setColumn(1);
+//		}
+//	}
+//	/*else if ((currentSpace.getRow() == boardSize) && (currentSpace.getColumn() == boardSize) && (filled == boardSize - 1))
+//	{
+//		std::cout << "No Solution Found. " << endl;
+//		return;
+//	}*/
+//	else if (currentSpace.getColumn() >= boardSize)
+//	{
+//		currentSpace.setRow(filledSpaces.top().getRow());
+//		currentSpace.setColumn(filledSpaces.top().getColumn() + 1);
+//		filledSpaces.pop();
+//		filled--;
+//	}
+//	else if (currentSpace.getRow() <= boardSize)
+//	{
+//		currentSpace.setColumn(currentSpace.getColumn() + 1);
+//	}
+//}
+
+void n_Queens::displayBoard() const
+{
+	cout << boardSize << "-Queens Soulution." << endl;
+
+	/*std::cout << std::string(1, char(201));
+	for (int index = 0; index < boardSize - 1; index++)
+	{
+		std::cout << std::string(3, char(205));
+	}
+
+	for (int index = 0; index < boardSize; index++)
+	{
+		std::cout << std::string(1, char(186));
+		for (int j = 0; j < boardSize; j++)
+		{
+			std::cout << " " << " " << std::string(1, char(186));
+		}
+	}*/
+	system("pause");
+}
+
+bool checkIfConflict(int boardSize, stack<BoardSpaces> filledSpaces, BoardSpaces currentSpace)
+{
+	int stackSize = filledSpaces.size();
+
+	if (filledSpaces.size() > 1)
+	{
+		filledSpaces.pop();
+		if (abs(filledSpaces.top().getColumn() - currentSpace.getColumn()) <= 1)
+		{
+			return true;
+		}
+		else
+		{
+			while (!filledSpaces.empty())
+			{
+				if (filledSpaces.top().getColumn() == currentSpace.getColumn())
+				{
+					return true;
+				}
+				filledSpaces.pop();
+			}
+		}
+	}	
+
+	return false;
+}
+
+/*if (boardSize == 1)
+	{
+		return false;
+	}
+	if (abs(filledSpaces.top().getColumn() - currentSpace.getColumn()) <= 1)
+	{
+		return true;
+	}
+	else
+	{
+		while(!filledSpaces.empty())
+		{
+			if (filledSpaces.top().getColumn() == currentSpace.getColumn())
+			{
+
+				return true;
+			}
+			else
+			{
+				//filledSpaces.pop();
+			}
+		}
+	}*/
+
+void n_QueensMenu()
+{
+	n_Queens game;
+	game.fillBoard();
+	do
+	{		
+		break;
+
+	} while (true);
+
+}
---
+++ Assignment7-Stacks/n_Queens.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <stack>
+#include "BoardSpaces.h"
+
+using namespace std;
+
+class n_Queens : public BoardSpaces
+{
+private:
+
+	stack <BoardSpaces> filledSpaces;
+	char shiftDirection;
+	int firstColumnPermutaions;
+	int filled;
+	int boardSize;
+	bool gameWon;
+
+public:
+	n_Queens();
+	void setBoardSize(int newBoardSize);
+	void setFilled(int newFilledAmount);
+	int getBoardSize() const;
+	int getFilled() const;
+	void fillBoard();
+	void displayBoard() const; 
+};
+
+
